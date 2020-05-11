f1a3c053e867b0485f7fc47f569ece75aff0aa5e
chenitzan7@gmail.com <chenitzan7@gmail.com>
2020-05-11 11:41:07 +0300

    Added LeaveType Repositories  and CRUD and Added Datatables and Font Awsome Icons

---
+++ leave-management/Data/LeaveAllocation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class LeaveAllocation
+    {
+        [Key]
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; } //its a string because that way is define in the database
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+
+    }
+}
---
+++ leave-management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,57 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveType entity)
+        {
+            _db.LeaveTypes.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            _db.LeaveTypes.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+            return _db.LeaveTypes.ToList(); 
+        }
+
+        public LeaveType FindById(int id)
+        {
+            //_db.LeaveTypes.FirstOrDefault(n => n.Id == id);
+            return _db.LeaveTypes.Find(id);
+        }
+
+        public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            return  _db.SaveChanges() > 0; //at least one entity should be change! 
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            _db.LeaveTypes.Update(entity);
+            return Save();
+        }
+    }
+}
